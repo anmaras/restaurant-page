154270493b9c1088bacc20cb39371d87f73c67a2
Antonis <marasantonis@gmail.com>
2022-04-21 19:03:34 +0300

    add header style path

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import "/home/antonis/the_odin_project/restaurant-page/src/css/shared-style.css";
+import "../css/shared-style.css";
+import "../css/header-style.css";
 import pageLoad from "./pageLoad";
 
 document.body.appendChild(pageLoad());
