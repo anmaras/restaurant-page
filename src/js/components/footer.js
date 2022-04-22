f61dde6095a3c5bd335884dc25f0dc85d6cdfba0
Antonis <marasantonis@gmail.com>
2022-04-22 15:44:48 +0300

    footer component update

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,11 +1,21 @@
 import elementCreator from "../functions/elementCreator";
+import date from "../functions/footerDate";
 
 function footerComponent() {
   const footer = document.createElement("footer");
+  const dateSpan = elementCreator("span", "footer-date", date());
+  const iconSpan = elementCreator(
+    "img",
+    "footer-icon",
+    "",
+    "./src/icons/github.svg"
+  );
 
   footer.id = "footer";
 
-  footer.append(elementCreator("div", "footer-text", "Antonis Maras 2022"));
+  footer.append(elementCreator("div", "footer-text", `Antonis Maras`));
+
+  footer.childNodes[0].append(dateSpan, iconSpan);
 
   return footer;
 }
