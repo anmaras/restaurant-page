3d6af81ff7f81a5103dd98557537a039db5596c5
Antonis <marasantonis@gmail.com>
2022-04-26 17:39:39 +0300

    paths update

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -1,10 +1,11 @@
 import home from "../pages/home";
+import about from "../pages/about";
 
 function mainComponent() {
   const main = document.createElement("main");
   main.id = "main";
 
-  main.append(home());
+  main.append(about());
 
   return main;
 }
