addbbd03ee7194403d4a9360cb31fa2752da19b7
Antonis <marasantonis@gmail.com>
2022-04-26 17:38:58 +0300

    function condition update

--- src/js/functions/replaceContent.js
+++ src/js/functions/replaceContent.js
@@ -5,14 +5,16 @@ import menu from "../pages/menu";
 function replaceContent() {
   const MENU = "MENU";
   const HOME = "HOME";
+  const ABOUT = "ABOUT";
   let content = "";
 
   if (this.textContent === HOME) {
     content = home();
   } else if (this.textContent === MENU) {
     content = menu();
-  } else content = about();
-
+  } else if (this.textContent === ABOUT) {
+    content = about();
+  } else return;
   document.querySelector("#main").replaceChildren(content);
 }
 export default replaceContent;
