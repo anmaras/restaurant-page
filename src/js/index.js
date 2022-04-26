371b7224a4f3c4773c28ba4e0d1d754b88e79dc6
Antonis <marasantonis@gmail.com>
2022-04-26 17:34:38 +0300

    index paths update

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import "../css/shared-style.css";
 import "../css/header-style.css";
 import "../css/footer-style.css";
-import pageLoad from "./pageLoad";
+import "../css/home.css";
+import headerComponent from "./components/header";
+import mainComponent from "./components/main";
+import footerComponent from "./components/footer";
 
-document.body.appendChild(pageLoad());
+document.body.append(headerComponent(), mainComponent(), footerComponent());
