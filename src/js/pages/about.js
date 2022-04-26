b92fee2033aeaa97d399bffcf1738e81ec9f0f18
Antonis <marasantonis@gmail.com>
2022-04-26 17:36:05 +0300

    about imports update

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,10 +1,12 @@
+import elementCreator from "../functions/elementCreator";
+import firstSection from "./about-page/first-section";
+import secondSection from "./about-page/second-section";
+import thirdSection from "./about-page/third-section";
+
 function about() {
-  const about = document.createElement("div");
+  const about = elementCreator("div", "about-container");
 
-  about.className = "about-conteiner";
-
-  about.textContent = "ABOUT";
-
+  about.append(firstSection(), secondSection(), thirdSection());
   return about;
 }
 export default about;
