e69ffb4cd8a83ebb5a36bc8fcc292b3b6200a4ae
Antonis <marasantonis@gmail.com>
2022-04-26 11:12:15 +0300

    add date and icon to append function

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,21 +1,16 @@
 import elementCreator from "../functions/elementCreator";
 import date from "../functions/footerDate";
+import github from "/src/icons/github.svg";
 
 function footerComponent() {
   const footer = document.createElement("footer");
-  const dateSpan = elementCreator("span", "footer-date", date());
-  const iconSpan = elementCreator(
-    "img",
-    "footer-icon",
-    "",
-    "./src/icons/github.svg"
-  );
+  const dateSpan = elementCreator("div", "footer-date", date());
+  const iconSpan = elementCreator("img", "footer-icon", "", github);
+  const text = elementCreator("div", "footer-text", `Antonis Maras `);
 
   footer.id = "footer";
 
-  footer.append(elementCreator("div", "footer-text", `Antonis Maras`));
-
-  footer.childNodes[0].append(dateSpan, iconSpan);
+  footer.append(text, dateSpan, iconSpan);
 
   return footer;
 }
