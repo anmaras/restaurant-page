b1388dc64c752328cf5b8ce995b963158a2f4b0e
Antonis <marasantonis@gmail.com>
2022-04-27 21:46:15 +0300

    main component update

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -1,11 +1,12 @@
 import home from "../pages/home";
 import about from "../pages/about";
+import menu from "../pages/menu";
 
 function mainComponent() {
   const main = document.createElement("main");
   main.id = "main";
 
-  main.append(about());
+  main.append(menu());
 
   return main;
 }
