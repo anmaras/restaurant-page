6a3fe85732dff6ff855bdb18e647241044c31fc2
Antonis <marasantonis@gmail.com>
2022-04-29 12:47:45 +0300

    third section added

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -33,6 +33,49 @@ function secondSection() {
   return element;
 }
 
+function thirdSection() {
+  const element = elementCreator("section", "home-container_third-section");
+
+  const markup = `<div class="home-second-section_locations">
+      <div class="restaurant">
+        <div class="icon"></div>
+        <h2 class="title">VISIT OUR RESTAURANT</h2>
+        <div class="hours">
+          <p>MONDAY - THURSDAY 11AM – 10PM</p>
+          <p>FRIDAY - SUNDAY 11AM - 11PM</p>
+        </div>
+        <div class="link">
+          <a href="#">
+            <p>1183 Davie Street</p>
+            <p>Vancouver, BC V6E 1 N2</p>
+          </a>
+        </div>
+        <div class="btn">HOURS & LOCATION</div>
+      </div>
+    </div>
+    <div class="home-second-section_locations">
+      <div class="truck">
+        <div class="icon"></div>
+        <h2 class="title">VISIT OUR FOOD TRUCK</h2>
+        <div class="hours">
+          <p>MONDAY - FRIDAY 11AM-2PM AT ROBSON SQUARE</p>
+          <p>SATURDAY - SUNDAY AT EVENTS AROUND THE CITY</p>
+        </div>
+        <div class="link">
+          <a href="#">
+            <p>1183 Davie Street</p>
+            <p>Vancouver, BC V6E 1 N2</p>
+          </a>
+        </div>
+        <div class="btn">STREET FOOD APP</div>
+      </div>
+    </div>`;
+
+  element.insertAdjacentHTML("beforeend", markup);
+
+  return element;
+}
+
 /* function sectionElement() {
   let text = "MONDAY - THURSDAY 11AM – 10PM \n FRIDAY - SUNDAY 11AM - 11PM";
 
@@ -91,7 +134,7 @@ function fifthSection() {
 function home() {
   const container = elementCreator("div", "home-container");
 
-  container.append(firstSection(), secondSection());
+  container.append(firstSection(), secondSection(), thirdSection());
 
   return container;
 }
