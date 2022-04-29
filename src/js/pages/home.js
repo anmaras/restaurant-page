01e86ad4813ba98de193b248018dbb47ff58494c
Antonis <marasantonis@gmail.com>
2022-04-29 18:20:44 +0300

    home page update

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -36,101 +36,31 @@ function secondSection() {
 function thirdSection() {
   const element = elementCreator("section", "home-container_third-section");
 
-  const markup = `<div class="home-second-section_locations">
+  const markup = `<div class="home-third-section_locations">
       <div class="restaurant">
         <div class="icon"></div>
         <h2 class="title">VISIT OUR RESTAURANT</h2>
         <div class="hours">
-          <p>MONDAY - THURSDAY 11AM – 10PM</p>
-          <p>FRIDAY - SUNDAY 11AM - 11PM</p>
-        </div>
-        <div class="link">
-          <a href="#">
-            <p>1183 Davie Street</p>
-            <p>Vancouver, BC V6E 1 N2</p>
-          </a>
+          <p>MONDAY - THURSDAY <span> 11AM – 10PM </span></p>
+          <p>FRIDAY - SUNDAY  <span>11AM - 11PM</span></p>
         </div>
         <div class="btn">HOURS & LOCATION</div>
       </div>
-    </div>
-    <div class="home-second-section_locations">
       <div class="truck">
         <div class="icon"></div>
         <h2 class="title">VISIT OUR FOOD TRUCK</h2>
         <div class="hours">
-          <p>MONDAY - FRIDAY 11AM-2PM AT ROBSON SQUARE</p>
-          <p>SATURDAY - SUNDAY AT EVENTS AROUND THE CITY</p>
-        </div>
-        <div class="link">
-          <a href="#">
-            <p>1183 Davie Street</p>
-            <p>Vancouver, BC V6E 1 N2</p>
-          </a>
+          <p>MONDAY - FRIDAY 11AM-2PM <span>at robson square</span></p>
+          <p>SATURDAY -SUNDAY  <span>at events around the city</span></p>
         </div>
         <div class="btn">STREET FOOD APP</div>
-      </div>
-    </div>`;
+      </div>`;
 
   element.insertAdjacentHTML("beforeend", markup);
 
   return element;
 }
 
-/* function sectionElement() {
-  let text = "MONDAY - THURSDAY 11AM – 10PM \n FRIDAY - SUNDAY 11AM - 11PM";
-
-  const container = elementCreator("div", "constainer_visit-restaurant");
-  const logo = elementCreator("img", "logo", "", restauranLogo);
-
-  const title = elementCreator("h2", "title", "Visit Our Restaurant");
-  const hours = elementCreator(
-    "div",
-    "hours-container",
-    "MONDAY - THURSDAY 11AM – 10PM"
-  );
-
-  const address = elementCreator("a", "restauran-address");
-  address.setAttribute("href", "#");
-  const btn = elementCreator("button", "hours-btn", "HOURS & LOCATION");
-
-  container.append(logo, title, hours, address, btn);
-  return container;
-} */
-
-/* function secondSection() {
-  const element = elementCreator("div", "second-section");
-  const h2 = elementCreator(
-    "h2",
-    "second-section_title",
-    "SERVING REAL FOOD,FAST!"
-  );
-  const paragraph = elementCreator(
-    "p",
-    "second-section_p",
-    "We cook up bright, bold Korean dishes that are totally unique in Vancouver, you won’t find our flavors and colors anywhere else. And we serve it all hot, fast and in a format that’s affordable and comfortable for urban Vancouverites to enjoy."
-  );
-
-  element.append(h2, paragraph);
-
-  return element;
-} */
-/* 
-function thirdSection() {
-  const element = elementCreator("div", "third-section");
-
-  element.append(sectionElement());
-
-  return element;
-}
-function fourthSection() {
-  const element = elementCreator("div", "fourth-section");
-  return element;
-}
-function fifthSection() {
-  const element = elementCreator("div", "fifth-section");
-  return element;
-}
- */
 function home() {
   const container = elementCreator("div", "home-container");
 
