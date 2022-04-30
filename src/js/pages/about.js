25520af905fafdf1b25e750aecfe77c9b5777a3f
Antonis <marasantonis@gmail.com>
2022-04-30 20:52:45 +0300

    page update

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,16 +1,51 @@
-import elementCreator from "../functions/elementCreator";
-import about_firstSection from "./about-page/first-section";
-import about_secondSection from "./about-page/second-section";
-import about_thirdSection from "./about-page/third-section";
+import timeline from "/src/imgs/timeline2.png";
+
+const timelineText = [
+  {
+    title: `2014, Lower Mainland`,
+    text: `Coming from Seoul, we knew that we wanted to bring authentic Korean dishes to YVR, 
+           but with a Western twist. We started selling Korean tacos and burritos at our small trailer, 
+           driving all across Greater Vancouver, the Lower Mainland.`,
+  },
+  {
+    title: `2017, Vancouver`,
+    text: `The food truck hit the streets offering real food fast and 
+            has since become a favorite for foodies and food truck goers in Vancouver.`,
+  },
+  {
+    title: `2019, Davie Village`,
+    text: `The four-on-the-floor neighborhood was one on our radar for a brick-and-mortar shop 
+           because of the vibe and diversity in Davie Village.Disco Cheetah is the definition of a fast-casual Korean Cuisine 
+           that puts our heart and soul into what we cook and what we serve.`,
+  },
+];
+
+function aboutStoryText() {
+  let markup = "";
+  for (let i = 0; i < timelineText.length; i++) {
+    markup += `<h3 class="title">${timelineText[i].title}</h3>
+               <p class="text">${timelineText[i].text}</p>`;
+  }
+  return markup;
+}
 
 function about() {
-  const about = elementCreator("div", "about-container");
+  const about = document.createElement("div");
+  about.className = "about-container";
 
-  about.append(
-    about_firstSection(),
-    about_secondSection(),
-    about_thirdSection()
-  );
+  const markup = ` <section class="about-container_first-section">
+      <div class="img">summer</div>
+      <h1 class="title">ABOUT US</h1>
+    </section>
+
+    <section class="about-container_second-section">
+      <h2 class="title">OUR STORY</h2>
+      <img src="${timeline}" alt="test" class="img" />
+    </section>
+    <section class="about-container_third-section">${aboutStoryText()}</section>
+`;
+
+  about.insertAdjacentHTML("beforeend", markup);
   return about;
 }
 export default about;
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,70 +1,52 @@
-import elementCreator from "../functions/elementCreator";
-import truckLogo from "/src/imgs/truckLogo.png";
-import restauranLogo from "/src/imgs/restaurantLogo.png";
-
-function firstSection() {
-  const element = elementCreator("section", "home-container_first-section");
-
-  const markup = `<div class="home-container_first-section_title">
-  <h1>DISCO</h1>
-  <h2>CHEETAH</h2>
-  <h2>KOREAN GRILL</h2>
-  </div>`;
-
-  element.insertAdjacentHTML("beforeend", markup);
-  return element;
-}
-
-function secondSection() {
-  const element = elementCreator("section", "home-container_second-section");
-
-  const markup = `<div class="home-second-section_mission">
-  <h1>SERVING REAL FOOD, FAST!</h1>
-      <p>We cook up bright, bold Korean dishes 
-      that are totally unique in Vancouver, 
-      you won’t find our flavors and colors anywhere else. 
-      And we serve it all hot, 
-      fast and in a format that’s affordable and comfortable 
-      for urban Vancouverites to enjoy.</p>
-  </div>`;
-
-  element.insertAdjacentHTML("beforeend", markup);
-
-  return element;
-}
-
-function thirdSection() {
-  const element = elementCreator("section", "home-container_third-section");
-
-  const markup = `<div class="home-third-section_locations">
-      <div class="restaurant">
-        <div class="icon"></div>
-        <h2 class="title">VISIT OUR RESTAURANT</h2>
-        <div class="hours">
-          <p>MONDAY - THURSDAY <span> 11AM – 10PM </span></p>
-          <p>FRIDAY - SUNDAY  <span>11AM - 11PM</span></p>
-        </div>
-        <div class="btn">HOURS & LOCATION</div>
-      </div>
-      <div class="truck">
-        <div class="icon"></div>
-        <h2 class="title">VISIT OUR FOOD TRUCK</h2>
-        <div class="hours">
-          <p>MONDAY - FRIDAY 11AM-2PM <span>at robson square</span></p>
-          <p>SATURDAY -SUNDAY  <span>at events around the city</span></p>
-        </div>
-        <div class="btn">STREET FOOD APP</div>
-      </div>`;
-
-  element.insertAdjacentHTML("beforeend", markup);
-
-  return element;
-}
-
 function home() {
-  const container = elementCreator("div", "home-container");
+  const container = document.createElement("div");
+  container.className = "home-container";
 
-  container.append(firstSection(), secondSection(), thirdSection());
+  const markup = `
+<section class="home-container_first-section">
+      <div class="home-container_first-section_title">
+        <h1>DISCO</h1>
+        <h2>CHEETAH</h2>
+        <h2>KOREAN GRILL</h2>
+      </div>
+    </section>
+
+    <section class="home-container_second-section">
+      <div class="home-second-section_mission">
+        <h1>SERVING REAL FOOD, FAST!</h1>
+        <p>
+          We cook up bright, bold Korean dishes that are totally unique in
+          Vancouver, you won’t find our flavors and colors anywhere else. And we
+          serve it all hot, fast and in a format that’s affordable and
+          comfortable for urban Vancouverites to enjoy.
+        </p>
+      </div>
+    </section>
+    
+    <section class="home-container_third-section">
+      <div class="home-third-section_locations">
+        <div class="restaurant">
+          <div class="icon"></div>
+          <h2 class="title">VISIT OUR RESTAURANT</h2>
+          <div class="hours">
+            <p>MONDAY - THURSDAY <span> 11AM – 10PM </span></p>
+            <p>FRIDAY - SUNDAY <span>11AM - 11PM</span></p>
+          </div>
+          <div class="btn">HOURS & LOCATION</div>
+        </div>
+        <div class="truck">
+          <div class="icon"></div>
+          <h2 class="title">VISIT OUR FOOD TRUCK</h2>
+          <div class="hours">
+            <p>MONDAY - FRIDAY 11AM-2PM <span>at robson square</span></p>
+            <p>SATURDAY -SUNDAY <span>at events around the city</span></p>
+          </div>
+          <div class="btn">STREET FOOD APP</div>
+        </div>
+      </div>
+    </section>`;
+
+  container.insertAdjacentHTML("beforeend", markup);
 
   return container;
 }
