e4535fae23dbf9041188ff99af0a893af61e58e6
Antonis <marasantonis@gmail.com>
2022-04-30 20:51:48 +0300

    update page load path

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import "../css/about-style.css";
 import "../css/menu-style.css";
 import "../css/home.css";
 import headerComponent from "./components/header";
-import mainComponent from "./components/main";
+import mainComponent from "./pageLoad";
 import footerComponent from "./components/footer";
 
 document.body.append(headerComponent(), mainComponent(), footerComponent());
